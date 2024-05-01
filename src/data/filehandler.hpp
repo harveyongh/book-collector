bf6e2a0be69b6c613a610c83db37406bda8ea0b1
harvey desktop <harveygit@pm.me>
2024-05-01 17:29:39 -0500

    Placeholders for file handler and functions

---
+++ src/data/filehandler.hpp
@@ -0,0 +1,9 @@
+#ifndef FILEHANDLER_H_
+#define FILEHANDLER_H_
+#include <fstream>
+#include <string>
+
+// createLibrary function to read and build library object
+// writeLibrary function to write library obj to file
+
+#endif // FILEHANDLER_H_
