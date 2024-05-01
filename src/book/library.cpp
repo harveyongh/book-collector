669132e76f89685507083d4c5a282069977c9d39
harvey desktop <harveygit@pm.me>
2024-05-01 18:07:04 -0500

    function to delete book from library vector

--- src/book/library.cpp
+++ src/book/library.cpp
@@ -1,6 +1,13 @@
 #include "library.hpp"
+#include <algorithm>
 
 void BookLibrary::addBook(Book newBook)
 {
     books.push_back(newBook);
+}
+
+void BookLibrary::deleteBook(Book delBook)
+{
+    auto delIter = std::find(books.begin(), books.end(), delBook);
+    books.erase(delIter);
 }
