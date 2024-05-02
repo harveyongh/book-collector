f1131da42b89b42553fb6a11bc7f4a173a2554f8
harvey desktop <harveygit@pm.me>
2024-05-02 10:49:34 -0500

    Case statement manages menu interaction

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,56 @@
 #include "book/library.hpp"
 #include "data/filehandler.hpp"
 #include <iostream>
+#include <string.h>
 
 void mainMenu(bool);
 int main()
 {
     bool libraryOpenFlag = false;
+    BookLibrary currLibrary;
+    std::string fileName;
     mainMenu(libraryOpenFlag);
+    char option;
     // while loop to continue prompting for input while active
+    while (option != 'e'){
+        Book newBook;
+        BookLibrary queryLibrary;
+        std::string filterOpt = "name"; std::string filter;
+        switch (option) {
+            case 'm':
+                mainMenu(libraryOpenFlag);
+                break;
+            case 'o':
+                std::cout << "Please enter filename: ";
+                std::cin >> fileName;
+                currLibrary = createLibrary(fileName);
+                break;
+            case 'a':
+                newBook.newBook();
+                currLibrary.addBook(newBook);
+                break;
+            case 's':
+                queryLibrary.books = currLibrary.sort("");
+                queryLibrary.printLibrary();
+                break;
+            case 'f':
+                std::cout << "Please enter filter option, name/title: ";
+                std::cin >> filterOpt;
+                std::cout << "Please enter filter: ";
+                std::cin >> filter;
+                queryLibrary.books = currLibrary.filter(filterOpt, filter);
+                queryLibrary.printLibrary();
+                break;
+            case 'c':
+                writeLibrary(&currLibrary);
+                currLibrary.books.clear();
+                currLibrary.name = "";
+                break;
+        }
+        std::cout << "Enter menu option: ";
+        std::cin >> option;
+        std::cout << std::endl;
+    }
     // m reruns main menu with flag
     // o runs the filehandler and generates library object
     // a runs library addbook function
