ec8c0981a65427766d7f9d3ac6c619509299d9a2
harvey desktop <harveygit@pm.me>
2024-05-01 20:41:08 -0500

    Function creates new book library from file

--- src/data/filehandler.cpp
+++ src/data/filehandler.cpp
@@ -1,1 +1,23 @@
 #include "filehandler.hpp"
+#include <fstream>
+
+BookLibrary createLibrary(std::string fileName)
+{
+    std::ifstream fileInput (fileName);
+    std::string currLine;
+    BookLibrary fileLibrary;
+    fileLibrary.name = fileName;
+
+    getline(fileInput, currLine);// Discard column labels
+    while (getline(fileInput, currLine)){
+        Book parsedBook;
+        parsedBook.parseBook(currLine);
+        fileLibrary.addBook(parsedBook);
+    }
+    return fileLibrary;
+}
+
+void writeLibrary(BookLibrary *workingLibrary)
+{
+    //
+}
