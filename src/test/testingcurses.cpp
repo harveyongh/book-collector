afaf8989b67f769427e3ee4add81d016d4066383
Harvey Justice <harveygit@pm.me>
2024-09-20 13:22:23 -0500

    testing files building and working as intended

--- src/test/testingcurses.cpp
+++ src/test/testingcurses.cpp
@@ -1,5 +1,6 @@
 #include "../curses.hpp"
 #include <panel.h>
+#include <string>
 
 int main(int argc, char **argv)
 {
@@ -13,6 +14,17 @@ int main(int argc, char **argv)
     doupdate();
 
     getch(); // pause running
+
+    char response[32];
+    std::string prompt = "This is the prompt";
+
+    dialogPrompt(windowArray[0], prompt, response, 32);
+    wmove(windowArray[1], 2, 2);
+    wprintw(windowArray[1], "%s", response);
+
+    update_panels();
+    doupdate();
+    getch();
 
     endCurses(panelArray, windowArray);
 
